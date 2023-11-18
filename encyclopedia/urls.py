ad09e790cc97109114517de88710b4043360b5e2
odakris <christopher_denis@live.fr>
2023-11-19 00:08:02 +0100

    Add entry page handle like 'wiki/TITLE'

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/", views.index, name="index"),
+    path("wiki/<str:title>/", views.get_page, name="page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from markdown2 import Markdown
+
 
 from . import util
 
@@ -7,4 +9,18 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def get_page(request, title):
+    markdowner = Markdown()
+    
+    try:    
+        html = markdowner.convert(util.get_entry(title))
+        return render(request, "encyclopedia/page.html", {
+            "title": title,
+            "page": html
+        })
+    except:
+        return render(request, "encyclopedia/error.html", {
+            "title": title
+        })
 
