951896b8ec59fcc14c884431fd656d78daafb735
odakris <christopher_denis@live.fr>
2024-02-07 20:02:51 +0100

    update STATICFILES_DIRS

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 
 from . import views
 
@@ -15,6 +13,3 @@ urlpatterns = [
     path("delete/<str:title>/", views.delete_wiki, name="delete_wiki"),
     path("random/", views.random_wiki, name="random_wiki")
 ]
-
-# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
