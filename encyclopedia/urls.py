e4fc7847130d99cbecee3cc30aba5e8aa988c485
odakris <christopher_denis@live.fr>
2024-02-07 19:38:08 +0100

    update config for deployment on vercel

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -13,3 +15,6 @@ urlpatterns = [
     path("delete/<str:title>/", views.delete_wiki, name="delete_wiki"),
     path("random/", views.random_wiki, name="random_wiki")
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
